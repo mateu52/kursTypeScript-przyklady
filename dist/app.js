34ece15d6f85f54321470851ad840ab739a6de60
mateu52 <wuoelte@gmail.com>
2024-02-04 17:03:51 +0100

    union type

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,4 @@
-const input1Element = document.querySelector('#input1');
-const input2Element = document.querySelector('#input2');
-const addButtonElement = document.querySelector("button");
-const add = (v1, v2) => v1 + v2;
-addButtonElement.addEventListener("click", () => {
-    const sum = add(Number(input1Element.value), Number(input2Element.value));
-    console.log(sum);
-});
+const logAge = (age) => {
+    console.log(`hej mam ${age} lat`);
+};
+logAge(23);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,5 @@
-const input1Element: HTMLInputElement = document.querySelector('#input1');
-const input2Element: HTMLInputElement = document.querySelector('#input2');
-const addButtonElement = document.querySelector("button");
+const logAge = (age: number) => {
+    console.log(`hej mam ${age} lat`);
+}
 
-const add = (v1: number, v2: number) => v1 + v2;
-
-addButtonElement.addEventListener("click", () => {
-    const sum = add(Number(input1Element.value), Number(input2Element.value))
-    console.log(sum)
-})
+logAge(23)
